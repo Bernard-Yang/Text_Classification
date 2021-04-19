582ec2c468ec2be8524641e18386c93468a1fbf3
Bernard-Yang <45395508+Bernard-Yang@users.noreply.github.com>
2021-04-19 15:26:45 +0100

    Update tfidf_lightgbm_cv_baseline.py

--- tfidf_lightgbm_cv_baseline.py
+++ tfidf_lightgbm_cv_baseline.py
@@ -25,7 +25,7 @@ word_vec = TfidfVectorizer(analyzer='word',
 train_term_doc = word_vec.fit_transform(train_df['text_split'])
 test_term_doc = word_vec.transform(test_df['text_split'])
 
-# F1 score-线下
+# F1 score
 from sklearn.metrics import f1_score
 #[1,2,3,2,1,3]
 #[1,2,3,1,1,3]
@@ -35,13 +35,12 @@ def cal_macro_f1(y_true,y_pred):
 
 X_train, X_eval, y_train, y_eval  = train_test_split(train_term_doc,train_df['label'],test_size=0.2,shuffle=True,random_state=2019) # split the training data
 
-# CV
+# Cross Validation
 from sklearn.model_selection import KFold
 kf = KFold(n_splits=10, shuffle=True, random_state=666)
-train_matrix = np.zeros((train_df.shape[0],14)) #记录验证集的概率
-##!!!!!
-test_pre_matrix = np.zeros((10,test_df.shape[0],14)) #将5轮的测试概率分别保存起来
-cv_scores=[] #每一轮线下的验证成绩
+train_matrix = np.zeros((train_df.shape[0],14)) #prob for evaluation
+test_pre_matrix = np.zeros((10,test_df.shape[0],14)) #save test prob
+cv_scores=[] #cv scores
 
 import lightgbm as lgb
 from sklearn.linear_model import LogisticRegression
@@ -49,16 +48,15 @@ for i,(train_index,eval_index) in enumerate(kf.split(train_term_doc)):
     print(len(train_index),len(eval_index))
     
     
-    #训练集
+    #training set
     X_train = train_term_doc[train_index]
     y_train = train_df['label'][train_index]
     
-    #验证集
+    #validation set
     X_eval = train_term_doc[eval_index]
     y_eval = train_df['label'][eval_index]
     
-    # model = LogisticRegression(C=4, dual=False) 
-    # model.fit(X_train,y_train)
+
     model =lgb.LGBMClassifier(boosting_type='gbdt', 
                    num_leaves=2**5,
                    max_depth=-1, 
@@ -72,13 +70,12 @@ for i,(train_index,eval_index) in enumerate(kf.split(train_term_doc)):
                    num_class=19,#
                    silent=True, 
                    random_state=2019,
-#                    class_weight=20,
                    colsample_bylevel=0.5,
                    min_child_weight=1.5,
                    metric='multi_logloss'
                   )
     model.fit(X_train,y_train,eval_set=(X_eval,y_eval), early_stopping_rounds=100)
-    ####对于验证集进行预测
+    ####predict for validation set
     eval_prob = model.predict_proba(X_eval)
     train_matrix[eval_index] = eval_prob.reshape((X_eval.shape[0], 14))#array
     
@@ -87,7 +84,7 @@ for i,(train_index,eval_index) in enumerate(kf.split(train_term_doc)):
     cv_scores.append(score)
     print("validation score is",score)
     
-    ###对于测试集进行预测
+    ###predict for testing set
     test_prob = model.predict_proba(test_term_doc)
     test_pre_matrix[i,:,:] = test_prob.reshape((test_term_doc.shape[0], 14))
 
